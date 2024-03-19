37cc0e43a4a7b522e640b22d3d480be6f72d6546
toczekmj <toczekmj@gmail.com>
2024-03-19 15:12:30 +0100

    init comment movies branch

---
+++ Model/Comment.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Model;
+
+public class Comment
+{
+    [Key] public int CommentId { get; set; }
+    public int MovieId { get; set; }
+    public string Text { get; set; }
+}
